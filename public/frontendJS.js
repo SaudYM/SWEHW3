a525f38d9735684ab4175265c0582b55ca467c03
SaudYM <alyamisaud7@gmail.com>
2022-12-07 21:10:58 +0300

    test

--- public/frontendJS.js
+++ public/frontendJS.js
@@ -1,34 +1,34 @@
+function showComments(recipe_id) {
 
-const BASE_URL = 'http://localhost:8000/recipes/:recipe_id/comments'
-const querySelector = (selector) => {
-    return document.querySelector(selector)
-}
+    var Div = document.getElementById("cmntcontainer");
 
-async function showComments(recipe_id) {
+    fetch(`/recipes/${recipe_id}/comments`)
+        .then((res) => res.json())
+        .then((comments) => {
 
-    const listDiv = qs('#tasksList')
-    const response = await fetch(BASE_URL)
-    console.log(response)
-    if (response.ok) {
-        const comments = await response.json()
-        output = comments.map((cmnt) => `
-        <div class="box2">
-                <p>
-                    This is a very fun recipe to follow.</p>
-                <p> - Ahmed Z. </p>
-            </div>
-            <div class="box2">
-                <p>
-                    This is my favorite recipe. I enjoy it.</p>
-                <p> - Karim K. </p>
-            </div>
-        `).join('')
-    }
-    listDiv.innerHTML = output
-}
+            let commennts = comments
+                .map((com) => {
+                    return `
+                 <p>${com.comment}</p>
+                <p> - ${com.author} </p>
+        `;
+                })
+                .join("");
 
-function seeComments(recipe_id) { }
+            Div.innerHTML = qr;
+        });
 
-function sendComment(recipe_id) { }
-console.log(showComments())
-showComments();
+};
+
+function seeComments(recipe_id) {
+
+    document.getElementById('comments').style.display = block;
+    showComments(recipe_id)
+
+};
+
+function sendComment(recipe_id) {
+
+
+
+};
