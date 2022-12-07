eb97c4b2ec5a4147e9a82a10e5d66f8696a6a4b1
SaudYM <alyamisaud7@gmail.com>
2022-12-07 21:43:28 +0300

    final

--- public/frontendJS.js
+++ public/frontendJS.js
@@ -15,20 +15,32 @@ function showComments(recipe_id) {
                 })
                 .join("");
 
-            Div.innerHTML = qr;
+            Div.innerHTML = commennts;
         });
-
 };
-
 function seeComments(recipe_id) {
 
-    document.getElementById('comments').style.display = block;
+    document.getElementById('box').style.display = block;
     showComments(recipe_id)
 
 };
 
 function sendComment(recipe_id) {
 
+    let author = document.getElementById("name").value;
+    let comment = document.getElementById("cmnt").value;
+    let cmntObj = { author, comment };
+    fetch(`/recipes/${recipe_id}/comments`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cmntObj),
+    })
+        .then((response) => response.json())
+        .then(() => {
+            authorInput.value = "";
+            commentInput.value = "";
+            showComments(recipe_id);
+        });
 
 
 };
